c171cd8b477229a027da7e375097b38ff809d550
xXMRK888YTXx <aksehchikov@gmail.com>
2023-11-07 20:20:31 +0300

    [Feature] Added font

---
+++ app/src/main/java/com/xxmrk888ytxx/bit_cup_2023/core/presentation/theme/ApplicationFont.kt
@@ -0,0 +1,12 @@
+package com.xxmrk888ytxx.bit_cup_2023.core.presentation.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.xxmrk888ytxx.bit_cup_2023.R
+
+object ApplicationFont {
+    val mulish = FontFamily(
+        Font(R.font.mulish_regular, weight = FontWeight.W400)
+    )
+}

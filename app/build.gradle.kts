d440b71f076cab595bd4d96d62b5c2d89d2c52ff
xXMRK888YTXx <aksehchikov@gmail.com>
2023-11-11 14:47:21 +0300

    [Feature] Changed min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.xxmrk888ytxx.bit_cup_2023"
-        minSdk = 26
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
